ff636b3da24a8c53796468fba885fa882acf7ed6
Angela <AngelaVargas>
2017-03-28 21:14:42 +0200

    App CMS

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pages
 
 # Create your views here.
+
+def index(request, peticion):
+	print(peticion)
+	try:
+		Request = Pages.objects.get(name=peticion)
+	except Pages.DoesNotExist:
+		return HttpResponse('Page Not Found')
+
+	Response = "Hello, I'm " + Request.name + " : " + str(Request.page)
+
+	return HttpResponse(Response)
